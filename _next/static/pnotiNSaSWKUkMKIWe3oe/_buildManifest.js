8336d2102446d5361dacf4430945c8982d4458b2
s3igo <s3igo@users.noreply.github.com>
2023-03-15 03:07:14 +0000

    deploy: f9a68f4bae87fb1d77299f07f8acbcc86bf5ede7

---
+++ _next/static/pnotiNSaSWKUkMKIWe3oe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/729-5496ee228cc2e59175db.js","static/chunks/pages/index-3b9987b8166ca8bf0f9c.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
